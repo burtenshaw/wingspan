fa3c1b7cad3cad849afd91736f0d4b5e6b04d508
Burtenshaw <ben.burtenshaw@gmail.com>
2020-12-29 16:30:46 +0100

    tidy

--- RUN_CATEGORICAL.sh
+++
@@ -1,8 +0,0 @@
-
-tensorboard --logdir logs/categorical --port 6009 & \
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py start & \
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py end 
-
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py spans & \
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py len
-
