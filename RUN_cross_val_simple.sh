fb127a394ceb28717b80135b8f78151e2ee5e35b
burtenshaw <ben.burtenshaw@gmail.com>
2021-06-29 20:44:35 +0200

    for publication

--- RUN_cross_val_simple.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py --method_name end --fold $run
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py --method_name start --fold $run 
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py --method_name spans --fold $run
-CUDA_VISIBLE_DEVICES=1 python train_categorical.py --method_name len --fold $run
-    
-
--- RUN_gpu_0.sh
+++
@@ -1,7 +0,0 @@
-# CUDA_VISIBLE_DEVICES=0 python train_bert_ngram.py --method_name bert_ngram --hparams --runs 10
-# CUDA_VISIBLE_DEVICES=0 python train_bert_span.py --method_name bert_span
-# # CUDA_VISIBLE_DEVICES=0 python train_lstm_ngram.py --method_name lstm_ngram
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py --method_name end --hparams --runs 10
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py --method_name start --hparams --runs 10
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py --method_name spans --hparams --runs 10
-CUDA_VISIBLE_DEVICES=0 python train_categorical.py --method_name len --hparams --runs 10
